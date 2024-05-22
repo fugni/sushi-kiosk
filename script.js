d11452c62925c55663f8f541a225ef5df6034b12
fungi <sylvienuijtemans@gmail.com>
2024-05-22 09:22:29 +0200

    updated frontend + api

---
+++ script.js
@@ -0,0 +1,59 @@
+var selectedCategory = "sushi";
+
+function category(selectedCategory) {
+    // clear container
+    var container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    fetch("https://u220722.gluwebsite.nl/products/" + selectedCategory)
+        .then(response => response.json())
+        .then(data => data.forEach(element => {
+            newItem(element);
+        }));
+
+}
+
+function newItem(element) {
+    var container = document.querySelector(".container");
+    var item = document.createElement("div");
+    item.classList.add("item");
+
+    var img = document.createElement("img");
+    img.src = "/assets/img/sushi/" + element.Image;
+
+    var name = document.createElement("p");
+    name.innerHTML = element.Name;
+
+    var amount = document.createElement("p");
+    amount.innerHTML = element.Amount;
+
+    switch (element.Type) {
+        case "dranken":
+            amount.innerHTML += "ml";
+            break;
+        default:
+            amount.innerHTML += " stuks";
+            break;
+    }
+
+    var price = document.createElement("p");
+    price.innerHTML = "€" + element.Price;
+
+    if (price.innerHTML.split(".")[1].length == 1) {
+        price.innerHTML += "0";
+    }
+
+
+    var toevoegen = document.createElement("div");
+    toevoegen.classList.add("toevoegen");
+    toevoegen.innerHTML = "Toevoegen";
+
+    item.appendChild(img);
+    item.appendChild(name);
+    item.appendChild(amount);
+    item.appendChild(price);
+    item.appendChild(toevoegen);
+    container.appendChild(item);
+}
+
+category(selectedCategory);
